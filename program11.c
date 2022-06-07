08f0b1f94e0f63075a845112e372cbe55caebb00
ArvindgChavan <106027395+ArvindgChavan@users.noreply.github.com>
2022-06-07 09:19:17 +0530

    Update program11.c

--- program11.c
+++ program11.c
@@ -3,15 +3,12 @@
 #include<stdio.h>
 void Display()
 {
- int iCnt=0;
- for(iCnt=1;iCnt<=5;iCnt++)
-{
-
-printf("Hello\n");
+  int iCnt=0;
+  for(iCnt=1;iCnt<=5;iCnt++)
+   {
+     printf("Hello\n");
+   }
 }
-
-}
-
 int main()
 {
 Display();
